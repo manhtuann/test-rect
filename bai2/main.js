226571cd16ee4ac2ea4c97ebd47447d65dea054b
manhtuan <pmtss2k1@gmail.com>
2022-07-23 21:47:42 +0700

    [updateb2]

--- bai2/main.js
+++ bai2/main.js
@@ -1,10 +1,16 @@
 const arr = [60,40,55,75,64];
 let sTeam1 = 0;
 let sTeam2 = 0;
-function sumArr(index){
+let result = [];
+function sumArr(){
     for(let i = 0; i<arr.length; i++){
-        console.log(arr[i].index);
+        if(i%2 == 0 ){
+            sTeam1 +=arr[i]
+        }
+        else 
+        sTeam2 += arr[i]
     }
-    
+    result.push(sTeam1,sTeam2)
+    console.log(result);
 }
 sumArr(arr)
